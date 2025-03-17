16c2c578d4fc6136f4e8b92ab044267732dffd0f
Andrew Kungu <drewskungu@gmail.com>
2025-03-17 20:57:02 +0300

    group

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(Ralph) {
+    cats.push("Ralph");
+    return cats 
+    
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob")
+}
+function  destructivelyRemoveLastCat(){
+    cats.pop()
+} 
+function  destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(name){
+    return  [...cats, "Broom" ];
+}
+function   prependCat(name){
+    return  ["Arnold",...cats ];
+}
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+function removeFirstCat() {
+    return cats.slice(1);
+}
